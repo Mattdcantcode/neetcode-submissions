2a5b393ad1db6cad2206dc86e52e0e10b70af595
Matthew Diplock <mattgdiplock@gmail.com>
2026-08-17 11:05:46 +0100

    Add: palindrome-number - submission-2

---
+++ Data Structures & Algorithms/palindrome-number/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        n = len(s)
+        a = []
+        for i in range(n):
+            a.append(s[-1-i]) 
+            print(a)
+        if a == list(s) or n == 1:
+            return True
+        else: 
+            return False
